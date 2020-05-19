2e81b40cfa5c65d6489e0fe7bb9a78afb9c7ea01
Jude Okagu <juud0010@gmail.com>
2020-05-20 01:02:23 +0200

    firts

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <nav>
+          <h2>Shopping Cart</h2>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Store</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
