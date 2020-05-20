9026e3178da4e5094d0e74ab94c02eac12dd12e3
Jude Okagu <juud0010@gmail.com>
2020-05-20 18:09:03 +0200

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import grayshirt from '../images/grayshirt.jpg';
+import blueshirt from '../images/blueshirt.jpeg';
+import redshirt from '../images/redshirt.jpg';
+import whiteshirt from '../images/whiteshirt.jpeg';
+import blackshirt from '../images/blackshirt.jpeg';
 
 const Home = () => {
   return (
-    <div>
-      <h2>building this next</h2>
+    <div className='container'>
+      <div className='image'>
+        <img src={grayshirt} alt='Gray Shirt' />
+        <h3>Gray Shirt</h3>
+        <h3>$15.00</h3>
+        <a className='addToCart cart1' href='#'>
+          Add to cart{' '}
+        </a>
+      </div>
+
+      <div className='image'>
+        <img src={whiteshirt} alt='White Shirt' />
+        <h3>white Shirt</h3>
+        <h3>$10.00</h3>
+        <a className='addToCart cart2' href='#'>
+          Add to cart{' '}
+        </a>
+      </div>
+
+      <div className='image'>
+        <img src={blueshirt} alt='Blue Shirt' />
+        <h3>Blue Shirt</h3>
+        <h3>$13.00</h3>
+        <a className='addToCart cart3' href='#'>
+          Add to cart{' '}
+        </a>
+      </div>
+
+      <div className='image'>
+        <img src={redshirt} alt='Red Shirt' />
+        <h3>Red Shirt</h3>
+        <h3>$12.50</h3>
+        <a className='addToCart cart4' href='#'>
+          Add to cart{' '}
+        </a>
+      </div>
+
+      <div className='image'>
+        <img src={blackshirt} alt='' />
+        <h3>Black Shirt</h3>
+        <h3>$17.20</h3>
+        <a className='addToCart cart5' href='#'>
+          Add to cart{' '}
+        </a>
+      </div>
     </div>
   );
 };
